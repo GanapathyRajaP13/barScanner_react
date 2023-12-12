d4a1b19fbf1c1484965f5c8578187ddd7a580e3a
GanapathyRajaP13 <123737552+GanapathyRajaP13@users.noreply.github.com>
2023-12-12 19:05:26 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://CHEMSMESTEST01:5500'
-    //baseURL: 'http://CHEWS10NB0008:5500'
+    baseURL: 'http://localhost:5500'
 });
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
